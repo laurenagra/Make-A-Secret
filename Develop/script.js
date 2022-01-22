c7f5d6023249eeae52195c24db7333898a0ec4f0
Lauren A <laurenhaileyama@gmail.com>
2022-01-22 14:30:54 -0500

    Create the readme, prompt the user to pick a number if original was outside of range

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,3 @@
-var alphaLow = "abcdefghijklmnopqrstuvwxyz";
-var alphaUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numbers = "0123456789";
-var symbols = "!@#$%^&*_-+=";
-
-
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -12,31 +5,99 @@ var generateBtn = document.querySelector("#generate");
 //Create my generate password function
 function generatePassword(){
   console.log("You clicked the button!");
+  //possible inclusions
+  var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var numbArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var characterArray = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+
+
+  //empty arrays
+  var resultArray = [];
+  var userArray = [];
 
 // I. Prompt the user for password criteria
 //     a. pasword length 8 < 128
   var click = window.prompt("How many characters would you like?")
+  var length = Number(click);
+  console.log(length);
 
-if (click < 8){
-  return ("Please choose a longer password length!")
-}
-else if (click > 128) {
-  return ("Please choose a shorter password length!")
-}
-else alert ("You chose " + click + " characters");
+  if (length < 8){
+    alert ("Please choose a longer password length!")
+    click = window.prompt("Please choose a length between 8 and 128 characters!");
+  }
+  else if (length > 128) {
+    alert ("Please choose a shorter password length!")
+    click = window.prompt("Please choose a length between 8 and 128 characters!");
+  }
+  else {
+    alert ("You chose " + length + " characters");
+  }
 //     b. lowercase, uppercase, numbers, special characters
 
-var low = confirm("Would you like to include lowercase letters?")
-if (low) {
+  var uppercases = confirm("Would you like to use uppercase letters?");
+
+  if (uppercases) {
+    alert ("Your password will contain uppercase letters");
+  } 
+  else {
+    alert ("Your password will not contain uppercase letters");
+  };
+
+  var lowercases = confirm("Would you like to use lowercase letters?");
+  if (lowercases) {
+    alert ("Your password will contain lowercase letters");
+  } else {
+    alert ("Your password will not contain lowercase letters");
+  };
+  
+  var numbers = confirm("Would you like to use numbers?");
+  if (numbers) {
+    alert ("Your password will contain numbers");
+  } else ("Your passworkd will not contain numbers");
+
+  var characters = confirm("Would you like to use special characters?"); 
+  if (characters) {
+    alert ("Your password will contain symbols");
+  } else ("Your password will not contain symbols");
+
+  while (!uppercases && !lowercases && !numbers && !characters) {
+    alert("You must select at least one character type!");
+    uppercases = confirm("Would you like to use uppercase letters?");
+    lowercases = confirm("Would you like to use lowercase letters?");
+    numbers = confirm("Would you like to use numbers?");
+    characters = confirm("Would you like to use special characters?");
+  }
+
+
+// Generate password based on criteria
+if (numbers){
+  resultArray = resultArray.concat(numbArray);
+  
+}
+
+if (uppercases){
+  resultArray = resultArray.concat(uppercaseArray);
 
 }
-// II. Validate the input
-// III. Generate password based on criteria
+
+if (lowercases){
+  resultArray = resultArray.concat(lowercaseArray);
+
+}
+
+if (characters){
+  resultArray = resultArray.concat(characterArray);
+}
+console.log(resultArray)
 
 
-
-//Display password to the page
-  return ("Generated Password will go here!")
+for (var i = 0; i < length; i++) {
+      
+  userArray.push (resultArray[Math.floor(Math.random() * resultArray.length)]); 
+}
+  console.log(userArray.join(""));
+  return userArray.join("") ;
 }
 
 
@@ -48,6 +109,7 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # Make-A-Secret
 
+## This project was built with the intention of getting a better insight into javascript capabilities, particularly random generation and concatentation possibilities. 
 
+>The point of this process was to really get a better understanding of how to contatenate within my arrays, and how to tie everything back in so values would be accounted for if needed. Initially, I wasn't sure how to get the actual password to print on the screen despite having all of the prompts for the password itself and all the alerts needed to identify the various parameters set by the user. 
+
+
+>It was a struggle to identify at first that I had to *change the strings into numbers,* and then better identify how lengths were being used within the parameters. Initially, I also forgot to loop back around for the password length itself. The system would prompt for a number, and if that number were outside of the allowed length *(8-128)*, then it would return a message asking the user to correct the number to within range, but then it would move into the next question instead of looping back to reask for a length, so I had to *add another alert within my if-statement that would prompt the user to input a new number if the number selected was not within range.*
+
+
+**This Project is able to generate a secure password that fits the needs of the user according to data inputted prior to generation**
 
 ## Solved: https://laurenagra.github.io/Make-A-Secret/
+### Thank you!
+
+<img src="./assets/images/password-gen.png">
